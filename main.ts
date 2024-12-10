c663e2630fc5f1575ccb7ea8529d96c3f55dee59
maxmurra <maximilian.murray@icloud.com>
2024-12-10 12:54:03 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    turtle.turnLeft()
+})
+input.onButtonPressed(Button.AB, function () {
+    turtle.forward(1)
+})
+input.onButtonPressed(Button.B, function () {
+    turtle.turnRight()
+})
+turtle.setPosition(2, 2)
+basic.forever(function () {
+	
+})
